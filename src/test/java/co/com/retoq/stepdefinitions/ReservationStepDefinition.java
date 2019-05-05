06ac60e0e5a7a02bd505736af3dfd94f26d9bdda
damianvengoechea <ldsdamian@hotmail.com>
2019-05-05 17:31:25 -0500

    Afinamiento Questions

--- src/test/java/co/com/retoq/stepdefinitions/ReservationStepDefinition.java
+++ src/test/java/co/com/retoq/stepdefinitions/ReservationStepDefinition.java
@@ -1,5 +1,8 @@
 package co.com.retoq.stepdefinitions;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.openqa.selenium.WebDriver;
 
 import co.com.retoq.questions.Reserver;
@@ -11,13 +14,9 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import net.bytebuddy.implementation.bytecode.assign.Assigner.EqualTypesOnly;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.thucydides.core.annotations.Managed;
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 public class ReservationStepDefinition {
 
