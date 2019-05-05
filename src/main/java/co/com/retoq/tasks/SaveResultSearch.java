ed2577e010ec388ae46a6bb05e8669c8b13b10b4
damianvengoechea <ldsdamian@hotmail.com>
2019-05-05 09:45:18 -0500

    Refactor de la clase SaveResultSearch

--- src/main/java/co/com/retoq/tasks/SaveResultSearch.java
+++ src/main/java/co/com/retoq/tasks/SaveResultSearch.java
@@ -27,40 +27,56 @@ public class SaveResultSearch implements Task{
 	        int daysHotel = utilString.splitNightSleep(ResultSearch.DAYS_IN_HOTEL.resolveFor(actor).getText()) + 1;
 	        int numHotels = utilString.splitnumHotels(ResultSearch.CAPTURE_NUM_HOTELS.resolveFor(actor).getText()) + 3;
 
-	        	while(i <= numHotels) {
-	        			// while(!ResultSearch.NEXT_BUTTON_NO_CLICKABLE.resolveFor(actor).isVisible()){     			
-	        	      		
-					if(ResultSearch.CAPTURE_NAME_HOTEL.of(String.valueOf(j)).resolveFor(actor).isPresent()) {
-						String nameHotel = ResultSearch.CAPTURE_NAME_HOTEL.of(String.valueOf(j)).resolveFor(actor).getText();
-						String priceHotel = ResultSearch.CAPTURE_PRICE_HOTEL.of(String.valueOf(j)).resolveFor(actor).getText();
-						realPriceHotel = (utilString.splitString(priceHotel) * daysHotel);
-						reservation.lstHotels.add(nameHotel);
-						reservation.lstPrices.add(realPriceHotel);
-						
-						log.info("------Hotel "+ j );
-						log.info(nameHotel);
-						log.info(realPriceHotel);
-						j++;						
-					}else{						
-						actor.wasAbleTo(Click.on(ResultSearch.NEXT_BUTTON));
-						log.info("cambio de Pagina ");
-						j = 1;
-					}
-					
-					i++;
-					
-	        		}
-	        
-	          
-				reservation.getLstPrices();
+	        	
+	        if(ResultSearch.NEXT_BUTTON.resolveFor(actor).isVisible()) {
+	        	
+	        	  while(i <= numHotels) {
+		        		// while(!ResultSearch.NEXT_BUTTON_NO_CLICKABLE.resolveFor(actor).isVisible()){     			
+		        	    if(ResultSearch.CAPTURE_NAME_HOTEL.of(String.valueOf(j)).resolveFor(actor).isPresent()) {
+		        		
+							String nameHotel = ResultSearch.CAPTURE_NAME_HOTEL.of(String.valueOf(j)).resolveFor(actor).getText();
+							String priceHotel = ResultSearch.CAPTURE_PRICE_HOTEL.of(String.valueOf(j)).resolveFor(actor).getText();
+							realPriceHotel = (utilString.splitString(priceHotel) * daysHotel);
+							reservation.lstHotels.add(nameHotel);
+							reservation.lstPrices.add(realPriceHotel);
+							log.info("......................");
+							log.info("Hotel: "+ nameHotel);
+							log.info("Price Total: " + realPriceHotel);
+							j++;						
+						}else if(ResultSearch.NEXT_BUTTON.resolveFor(actor).isVisible()){						
+									actor.wasAbleTo(Click.on(ResultSearch.NEXT_BUTTON));	
+									j = 1;
+								}else {
+									break;
+								}
+	        	  		}						
+						i++;
+						}else {
+							while(i <= numHotels) {
+								// while(!ResultSearch.NEXT_BUTTON_NO_CLICKABLE.resolveFor(actor).isVisible()){     			
+								if(ResultSearch.CAPTURE_NAME_HOTEL.of(String.valueOf(j)).resolveFor(actor).isPresent()) {
+									String nameHotel = ResultSearch.CAPTURE_NAME_HOTEL.of(String.valueOf(j)).resolveFor(actor).getText();
+									String priceHotel = ResultSearch.CAPTURE_PRICE_HOTEL.of(String.valueOf(j)).resolveFor(actor).getText();
+									realPriceHotel = (utilString.splitString(priceHotel) * daysHotel);
+									reservation.lstHotels.add(nameHotel);
+									reservation.lstPrices.add(realPriceHotel);							
+									log.info("....................");
+									log.info("Hotel: "+ nameHotel);
+									log.info("Price Total: " + realPriceHotel);
+									j++;						
+								}						
+						i++;
+						}	        
+	        }
+	        	reservation.getLstPrices();
 				reservation.getLstHotels();
-				log.info(reservation.getLstHotels());
 				String lessPrice = utilString.valueInList(reservation.getLstPrices());
 	            String[] partOfString = lessPrice.split(" ");
 	            String part1 = partOfString[0];
 	            String part2 = partOfString[1];
 	            actor.remember(ResultSearch.PRICE_HOTEL_CONSTANT, part1);
 	            actor.remember(ResultSearch.NAME_HOTEL_CONSTANT,reservation.getLstHotels().get(Integer.parseInt(part2)));
+	           
 	}
 	
 	
